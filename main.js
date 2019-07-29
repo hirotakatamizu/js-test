359487ed00f9a49194a99ade0147c38c091700cb
hirotaatamizu <hiro.music.is.life@gmail.com>
2019-07-29 11:21:39 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+var fruits = ['apple', 'apricot', 'avocado', 'blueberry', 'cherry', 'coconut', 'cranberry', 'dragonfruit', 'durian', 'grape', 'grapefruit', 'guava', 'kiwi fruit', 'lemon', 'lime', 'lychee', 'mango', 'melon', 'watermelon', 'miracle fruit', 'orange', 'bloodorange','clementine','mandarine','tangerine','papaya','passionfruit','peach','pear','persimmon','physalis','plum/prune','pineapple','pomegranate','raspberry','rambutan','star fruit','strawberry'];
+
+(function($, window) {
+  $(function() {
+    var list = $("#list");
+
+    function appendList(word) {
+      var item = $('<li class="list">').append(word);
+      list.append(item);
+    }
+
+    $("#keyword").on("keyup", function() {
+      var input = $("#keyword").val();
+      var reg = new RegExp("^" + input);
+
+      $(".list").remove();
+      if(input.length !== 0) {
+        $.each(fruits, function(i, fruit) {
+          if(fruit.match(reg)) {
+            appendList(fruit);
+          }
+        });
+
+      if ($(".list").length === 0) {
+      $("#result").text("一致するものはありませんでした。");
+        }
+      }
+      $('#list li').on("click", function() {
+        var text = $(this).text();
+        $('#keyword').val(text);
+        $(".list").remove();
+      });
+    });
+  });
+})(jQuery, window);
